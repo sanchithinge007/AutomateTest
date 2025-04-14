3a0c08b2bec6655049af678d1e6b16713b793f43
sanchithinge007 <47769032+sanchithinge007@users.noreply.github.com>
2025-04-14 22:54:06 +0530

    test

--- Automation/src/test/java/StepDefination/Login.java
+++ Automation/src/test/java/StepDefination/Login.java
@@ -3,13 +3,17 @@ package StepDefination;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import static org.slf4j.event.Level.INFO;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Login {
 	WebDriver driver;
+	private static final Logger LOW_LEVEL_LOGGER = LoggerFactory.getLogger(Login.class);
 	@Given("open the demo website")
 	public void open_the_demo_website() {
 		
@@ -19,7 +23,8 @@ public class Login {
 
 	@Then("enter userid {string} and password {string}")
 	public void enter_userid_and_password(String string, String string2) {
-	    driver.get("https://practice.expandtesting.com/register");    
+	    driver.get("https://practice.expandtesting.com/register");
+	    LOW_LEVEL_LOGGER.info("Hi");
 	    driver.findElement(By.xpath("//input[@id='username']")).sendKeys("sanchit");
 	    driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Hinge");
 	    try {
