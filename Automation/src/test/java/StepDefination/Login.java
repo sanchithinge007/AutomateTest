a90666bcee1eb5ee4f3f0326270fb3fc1f027ec8
sanchithinge007 <47769032+sanchithinge007@users.noreply.github.com>
2025-04-14 21:14:56 +0530

    test

--- Automation/src/test/java/StepDefination/Login.java
+++ Automation/src/test/java/StepDefination/Login.java
@@ -1,19 +1,30 @@
 package StepDefination;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Login {
-
+	WebDriver driver;
 	@Given("open the demo website")
 	public void open_the_demo_website() {
 		
-	    System.out.println("Hi");
+		WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
 	}
 
 	@Then("enter userid {string} and password {string}")
 	public void enter_userid_and_password(String string, String string2) {
-	    System.out.println("Login");
+	    driver.get("https://practice.expandtesting.com/register");
+	    try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Then("click on logout")
