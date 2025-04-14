63a78fbad81b8dac502af60cd8b2cb8cd59a0bdb
sanchithinge007 <47769032+sanchithinge007@users.noreply.github.com>
2025-04-14 20:37:06 +0530

    test

--- Automation/src/test/java/com/runner/TestNgRunner.java
+++ Automation/src/test/java/com/runner/TestNgRunner.java
@@ -1,10 +1,11 @@
 package com.runner;
-
-import io.cucumber.testng.CucumberOptions;
-
+import org.junit.runner.RunWith;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+@RunWith(Cucumber.class)
 @CucumberOptions(
 		features="src/test/resources/Features", 
-		glue="src/test/java/StepDefination",
+		glue={"StepDefination"},
 		plugin = {"pretty","html:target/cucumber-report.html","json:target/report.json"})
 public class TestNgRunner {
 	
