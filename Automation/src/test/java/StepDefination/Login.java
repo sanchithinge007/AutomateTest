59cc1defd6d1ab64833e98b6a79ec4cbf190f5d9
sanchithinge007 <47769032+sanchithinge007@users.noreply.github.com>
2025-04-15 21:45:31 +0530

    test

--- Automation/src/test/java/StepDefination/Login.java
+++ Automation/src/test/java/StepDefination/Login.java
@@ -1,12 +1,19 @@
 package StepDefination;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static org.slf4j.event.Level.INFO;
+
+import java.io.File;
+import java.io.IOException;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -23,16 +30,27 @@ public class Login {
 
 	@Then("enter userid {string} and password {string}")
 	public void enter_userid_and_password(String string, String string2) {
-	    driver.get("https://practice.expandtesting.com/register");
-	    LOW_LEVEL_LOGGER.info("Hi");
-	    driver.findElement(By.xpath("//input[@id='username']")).sendKeys("sanchit");
-	    driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Hinge");
+		File screenshot = null;
 	    try {
+	    	driver.get("https://practice.expandtesting.com/register");
+		    LOW_LEVEL_LOGGER.info("Hi");
+		    driver.findElement(By.xpath("//input[@id='username']")).sendKeys("sanchit");
+		    driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Hinge");
 			Thread.sleep(500);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+			
+			
+		}finally {
+			try {
+				screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+				FileUtils.copyFile(screenshot,new File("C:\\Users\\SanchitHinge\\.jenkins\\workspace\\Test1\\Automation\\failure.png"));
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+        }
 	}
 
 	@Then("click on logout")
